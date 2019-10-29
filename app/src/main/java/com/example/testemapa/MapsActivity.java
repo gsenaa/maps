22a1dd129bbf060cfd4ebddc0334bd3c16ecd359
gsenaa <47890401+gsenaa@users.noreply.github.com>
2019-10-29 17:31:20 -0300

    update 0.003

--- app/src/main/java/com/example/testemapa/MapsActivity.java
+++ app/src/main/java/com/example/testemapa/MapsActivity.java
@@ -35,6 +35,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
     private EditText mSearchText;
     private GoogleMap mMap;
 
@@ -158,7 +159,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(list.size() > 0){
             Address address = list.get(0);
             Log.d(TAG, "geoLocate: found a location: " + address.toString());
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM, address.getAddressLine(0));
         }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom, String title){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        MarkerOptions options = new MarkerOptions().position(latLng).title(title);
+        mMap.addMarker(options);
     }
 
 }
