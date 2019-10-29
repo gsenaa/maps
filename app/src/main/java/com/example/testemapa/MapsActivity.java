f375b829e23f45e93cba22a729055b6a20ebeb0c
gsenaa <47890401+gsenaa@users.noreply.github.com>
2019-10-29 16:23:26 -0300

    update 0.002

--- app/src/main/java/com/example/testemapa/MapsActivity.java
+++ app/src/main/java/com/example/testemapa/MapsActivity.java
@@ -1,15 +1,21 @@
 package com.example.testemapa;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +31,10 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapsActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private EditText mSearchText;
     private GoogleMap mMap;
 
@@ -36,7 +46,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        getLocationPermission();
+        mSearchText = (EditText) findViewById(R.id.input_search);
+    }
 
+
+    private Boolean mLocationPermissionsGranted = false;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        getLocationPermission();
+        mMap = googleMap;
+        if (mLocationPermissionsGranted = true){
+            init();
+        }
     }
 
     /**
@@ -49,10 +74,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
 
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MapsActivity.this);
+    }
+
+
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+            }else{
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+
+            }
+        }else{
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermissionsGranted = false;
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    for(int i = 0; i < grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionsGranted = true;
+                    //initialize our map
+                    initMap();
+                }
+            }
+        }
+    }
+
 
 
     private void init() {
-        Log.d("TAG", "init: initializing");
+        Log.d(TAG, "init: initializing");
         mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent keyEvent) {
@@ -70,7 +144,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     private void geoLocate(){
-        Log.d("TAG", "geoLocate: geolocating");
+        Log.d(TAG, "geoLocate: geoLocating");
 
         String searchString = mSearchText.getText().toString();
         Geocoder geocoder = new Geocoder(MapsActivity.this);
@@ -78,19 +152,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         try {
             list = geocoder.getFromLocationName(searchString, 1);
         }catch (IOException e){
-            Log.e("TAG", "geoLocate: IOException: " + e.getMessage());
+            Log.e(TAG, "geoLocate: IOException: " + e.getMessage());
         }
 
         if(list.size() > 0){
             Address address = list.get(0);
-            Log.d("TAG", "geoLocate: found a location: " + address.toString());
+            Log.d(TAG, "geoLocate: found a location: " + address.toString());
         }
     }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
-        Log.d("TAG", "onMapReady: map is ready");
-        mMap = googleMap;
 
-    }
 }
